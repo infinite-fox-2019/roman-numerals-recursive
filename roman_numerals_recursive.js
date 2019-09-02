cf5fe3d7928bb04b188cae15032182136f4c5f48
ItsJoel88 <imanuelnjodi@gmail.com>
2019-09-02 16:51:15 +0700

    menyelesaikan tugas roman-numerals-recursive

--- roman_numerals_recursive.js
+++ roman_numerals_recursive.js
@@ -1,6 +1,34 @@
 function toRoman(input) {
-  // start your code here
-  return to_roman(input);
+  var convertionTable = [
+    ['I',1],
+    ['IV',4],
+    ['V',5],
+    ['IX',9],
+    ['X',10],
+    ['XL',40],
+    ['L',50],
+    ['XC',90],
+    ['C',100],
+    ['CD',400],
+    ['D',500],
+    ['CM',900],
+    ['M',1000],
+  ]
+ 
+  if(input===0){
+      return ''
+  }else{
+      var output = ''
+      var countUp = 0
+      for(var i=convertionTable.length-1;i>=0;i--){
+          if(convertionTable[i][1]<=input - countUp){
+              output += convertionTable[i][0]
+              countUp += convertionTable[i][1]
+              return output + toRoman(input-countUp)
+          }
+      }
+      
+  }
 }
 
 console.log('My totally sweet testing script for new roman\n')
