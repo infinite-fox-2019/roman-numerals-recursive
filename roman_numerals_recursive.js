4232c0ad0970f25ad18d0478ada07a8f3af034eb
edwinsatya <edwinsatyayudistira@gmail.com>
2019-09-02 17:55:54 +0700

    roman rekursif done

--- roman_numerals_recursive.js
+++ roman_numerals_recursive.js
@@ -1,7 +1,20 @@
 function toRoman(input) {
   // start your code here
-  return to_roman(input);
-}
+  var roman = [['MM', 3000],['M', 1000],['CM', 900],['D', 500],['CD', 400],['CC', 200],['C', 100],['L', 50],['XL', 40],['X', 10],['IX', 9],['V', 5],['IV', 4],['I', 1]];
+  var result = '';
+  if (input === 0) {
+    return '';
+  } else {
+    for (var i = 0; i < roman.length; i++) {
+      if (input >= roman[i][1]) {
+          result += roman[i][0];
+          input -= roman[i][1];
+          break;
+      }
+    }
+  }
+  return result + toRoman(input);
+};
 
 console.log('My totally sweet testing script for new roman\n')
 console.log('input | expected | actual')
