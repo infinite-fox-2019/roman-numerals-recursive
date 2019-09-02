c2baab52dc7ac680e347307643a4f120d791caf7
elizabethwidiasri <elizabethwidiasri@gmail.com>
2019-09-02 21:18:19 +0700

    done task roman recursive

--- roman_numerals_recursive.js
+++ roman_numerals_recursive.js
@@ -1,15 +1,25 @@
 function toRoman(input) {
-  // start your code here
+  var roman = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
+  var arab = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+  if (input === 0) {
+    return '';
+  } else {
+    for (var i = 0; i < arab.length; i++) {
+      if (input % arab[i] < input) {
+        return roman[i] + toRoman(input - arab[i]);
+      }
+    }
+  }
   return to_roman(input);
 }
 
-console.log('My totally sweet testing script for new roman\n')
-console.log('input | expected | actual')
-console.log('——————|——————————|————————')
-console.log('4     | IV       | ', toRoman(4))
-console.log('9     | IX       | ', toRoman(9))
-console.log('23    | XXIII    | ', toRoman(23))
-console.log('1453  | MCDLIII  | ', toRoman(1453))
-console.log('1646  | MDCXLVI  | ', toRoman(1646))
+console.log('My totally sweet testing script for new roman\n');
+console.log('input | expected | actual');
+console.log('——————|——————————|————————');
+console.log('4     | IV       | ', toRoman(4));
+console.log('9     | IX       | ', toRoman(9));
+console.log('23    | XXIII    | ', toRoman(23));
+console.log('1453  | MCDLIII  | ', toRoman(1453));
+console.log('1646  | MDCXLVI  | ', toRoman(1646));
 
-module.exports = toRoman
+module.exports = toRoman;
