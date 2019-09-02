f308efd9fdc0eed00fc3a2e92cfcc5bed0bb347a
straf16 <depoksugutamu621@gmail.com>
2019-09-02 15:07:40 +0700

    mengerjakan roman numerals recursive

--- roman_numerals_recursive.js
+++ roman_numerals_recursive.js
@@ -1,6 +1,23 @@
 function toRoman(input) {
   // start your code here
-  return to_roman(input);
+  var arabic = [1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000];
+  var roman = ['I', 'IV', 'V', 'IX', 'X', 'XL', 'L', 'LC', 'C', 'CD', 'D', 'DM', 'M'];
+  var pengubah = 0;
+  var str = '';
+
+  if (input == 0) {
+    return '';
+  }
+  for (var i = arabic.length-1; i >= 0; i--) {
+    if (input >= arabic[i]) {
+      str += roman[i];
+      pengubah += arabic[i];
+      break;
+    }
+  }
+
+  var hasil = str + toRoman(input-pengubah);
+  return hasil;
 }
 
 console.log('My totally sweet testing script for new roman\n')
