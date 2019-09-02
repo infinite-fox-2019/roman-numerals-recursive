230a44068139a323c755b8c0f7a05f1f8dd642c7
Kevin Anantha <kevin.anantha@gmail.com>
2019-09-02 17:40:20 +0700

    :anchor:

--- roman_numerals_recursive.js
+++ roman_numerals_recursive.js
@@ -1,6 +1,32 @@
 function toRoman(input) {
   // start your code here
-  return to_roman(input);
+  const roman = {
+    M: 1000,
+    CM: 900,
+    D: 500,
+    CD: 400,
+    C: 100,
+    XC: 90,
+    L: 50,
+    XL: 40,
+    X: 10,
+    IX: 9,
+    V: 5,
+    IV: 4,
+    I: 1
+  }
+
+  let result = ''
+
+  if (!input) return result
+
+  for (let key in roman) {
+    if (input >= roman[key]) {
+      result += key
+      return result + toRoman(input - roman[key])
+    }
+  }
+
 }
 
 console.log('My totally sweet testing script for new roman\n')
